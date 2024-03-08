54a4224cba66c523ac0177a538568944bacb4b9b
ThomasLaforge <thomas.laforge.38@gmail.com>
2024-03-08 16:44:41 +0100

    with class

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,11 +4,8 @@ export default function Button() {
     const { darkTheme, switchTheme } = useDarkThemeStore()
     return (
         <button
-                style={{
-                    backgroundColor: darkTheme ? 'black' : 'white',
-                    color: darkTheme ? 'white' : 'black',
-                }} 
-                onClick={switchTheme}
-            >Change Theme</button>
+            className={"button"} 
+            onClick={switchTheme}
+        >Change Theme</button>
     );
 }
